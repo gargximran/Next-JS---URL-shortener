b9d7b92fba538f5dd62660abe1ac877d66846151
gargximran <gargximran@gmail.com>
2021-11-05 14:31:08 +0600

    first page done

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,93 @@
-import Head from 'next/head'
+import {useState, useRef, useEffect} from "react";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import axios from 'axios'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [url, setUrl] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [result, setResult] = useState('')
+    const r = useRef(null)
+    const l = useRef(null)
+    useEffect(() => {
+        r.current.focus()
+    }, [])
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    const submit = () => {
+        if(loading){
+            return false
+        }
+        setLoading(true)
+        setResult('')
+        setError("")
+        const pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if(!pattern.test(url)){
+            setError("URL is not correct!")
+        }
+        axios.post('/api/make-short-url', {url})
+            .then(res => {
+               setResult(location.href + res.data.data.slug)
+                setLoading(false)
+            })
+            .catch(err => {
+                setLoading(false)
+                setResult('')
+                setError("URL is not correct!")
+            })
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+    }
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+    return (
+        <div>
+            <h2 className="py-4 text-xl font-bold text-center bg-gray-400 text-black">URL Shortener</h2>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            <p className="text-lg italic text-blue-400 font-bold text-center mt-20">Enter URL Here <br/> <span
+                className={'text-sm text-gray-600 font-light'}>ex: https://google.com</span></p>
+            <div className="mx-5 md:mx-20 mt-5">
+                <input ref={r} type="text" placeholder={'https://www.example.com'} value={url}
+                       onChange={e => setUrl(e.target.value)}
+                       className="w-full shadow text-center rounded text-2xl font-bold py-3 px-4 border-2 focus:outline-none border-red-500"/>
+                <p className="text-center font-bold text-red-600">{error}</p>
+            </div>
+            <div className="flex justify-center py-3">
+                <button onClick={submit} className="px-10 py-2 rounded shadow bg-red-500 text-xl font-bold text-white">
+                    {
+                        loading ? (
+                            <>
+                                <svg className="animate-spin inline mr-1 -ml-1 mr-3 h-5 w-5 text-white"
+                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                            stroke-width="4"></circle>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                Loading
+                            </>
+                        ) : (
+                            'Submit'
+                        )
+                    }
+                    </button>
+            </div>
+            {
+                result ? (
+                    <div className="div py-10 text-center">
+                        <input ref={l} value={result}
+                               className="inline-block py-2 px-2 rounded bg-gray-400 font-bold text-lg shadow"/>
+                        <br/>
+                        <CopyToClipboard text={result} onCopy={() => {
+                            l.current.select()
+                            alert('URL copied to clipboard!')
+                        }}>
+                            <button className="bg-green-400 my-2 py-2 px-4 font-bold shadow">Copy to ClipBoard</button>
+                        </CopyToClipboard>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+                    </div>
+                ) : ''
+            }
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+
+
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
